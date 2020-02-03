21663c58b86081dd8db6eb941863a3f1724e4e1e
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 20:55:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+
+
+def roll_call_dwarves(array)
+  i = 0 
+   while i < array.length
+      puts "#{i + 1}. #{array[i]}"
+      i += 1 
+    end 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  empty_array =[]
+  i = 0 
+   while i < array.length
+      empty_array << #{array[i].capitalize} + "!"
+      i += 1 
+    end 
+    empty_array
 end
 
 def long_planeteer_calls# code an argument here
@@ -14,3 +26,5 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
+
+
