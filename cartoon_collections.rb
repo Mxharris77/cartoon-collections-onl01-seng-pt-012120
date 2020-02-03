5cb615d8ec396fd586a3ece723de94b14165b0ca
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 21:07:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -29,10 +29,12 @@ def long_planeteer_calls(array)
 
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   
-  cheese_types = ["cheddar", "gouda", "camembert"]
-  cheese_types.find{|i| i.odd?}
+  array = []
+  ["cheddar", "gouda", "camembert"].each do |i|
+  matches << i if i.even? # add i to the matches array if it is even
+end
 end
 
 
