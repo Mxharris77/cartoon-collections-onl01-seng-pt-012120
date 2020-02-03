b5b1daff15bfcd5cd4831e4f6bea3352007e8793
captivating-mapping-7374 <mxharris1094@yahoo.com>
2020-02-03 21:03:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -30,8 +30,9 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese# code an argument here
-  # the array below is here to help
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  .find{|i| i.odd?}
 end
 
 
